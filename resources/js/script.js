4d8b6fa438c032e338989cc2fe19430aa95bfd68
edsan <jackwest415@gmail.com>
2020-06-25 17:34:32 +0300

    adding the functionality of the sticky nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
-  $("h1").click(function () {
-    $(this).css("background-color", "#ff0000")
-  })
+  //3 and 3.2
+  $(".js--effects").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky")
+      } else {
+        $("nav").removeClass("sticky")
+      }
+    },
+    {
+      //3.3
+      offset: "6rem;",
+    }
+  )
 })
